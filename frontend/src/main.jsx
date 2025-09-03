81139a6387c6b894c59b6dc37e32f78b4a881787
vishubbk <vishubbkup@gmail.com>
2025-09-04 00:19:24 +0530

    #

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,20 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import Stairs from "./components/common/Stairs.jsx";
 import { NavProvider } from "./context/NavContext.jsx";
-
-
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <BrowserRouter>
-    <NavProvider>
-       <Stairs/> {/*this is my animation page load the  site */}
-      <App />
-    </NavProvider>
-    
-    </BrowserRouter>
+    <HashRouter>
+      <NavProvider>
+        <Stairs /> {/* this is my animation page load the site */}
+        <App />
+      </NavProvider>
+    </HashRouter>
   </StrictMode>
 );
