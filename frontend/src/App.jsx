4fb14ce2b5472b5a1c54317ac7faccdce6ffa2f8
vishubbk <vishubbkup@gmail.com>
2025-09-04 23:16:01 +0530

    #

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,28 @@
-import { HashRouter   as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/home.jsx'
-import Agence from './pages/agence.jsx'
-import Projects from './pages/projects.jsx'
-
-import FullScreenNav from './components/Navigation/FullScreenNav.jsx'
-import NotFound from './pages/NotFound.jsx'
-import Contact from './pages/Contact.jsx'
-import Blogs from './pages/Blogs.jsx'
-
+import Home from "./pages/home.jsx";
+import Agence from "./pages/agence.jsx";
+import Projects from "./pages/projects.jsx";
+import FullScreenNav from "./components/Navigation/FullScreenNav.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Contact from "./pages/Contact.jsx";
+import Blogs from "./pages/Blogs.jsx";
 
 const App = () => {
   return (
-  <Router>
-    <div className='overflow-x-hidden'>
-
-      <FullScreenNav/>
-      
+    <div className="overflow-x-hidden">
+      <FullScreenNav />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/FullScreen' element={<FullScreenNav/>}/>
-        <Route path='/Agency' element={<Agence/>}/>
-        <Route path='/Works' element={<Projects/>}/>
-        <Route path='/Contact' element={<Contact/>}/>
-        <Route path='/Blogs' element={<Blogs/>}/>
-        <Route path='/*' element={<NotFound/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/FullScreen" element={<FullScreenNav />} />
+        <Route path="/Agency" element={<Agence />} />
+        <Route path="/Works" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Blogs" element={<Blogs />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
-    </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
